40b689aa8fb7fbb0ef42d779d47c283df05fc117
ViniHorstFer <vinihf7@gmail.com>
2025-11-14 11:27:02 -0300

    Update indices_app.py

--- indices_app.py
+++ indices_app.py
@@ -11,25 +11,8 @@ from io import BytesIO, StringIO
 import warnings
 import yfinance as yf
 import calendar
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 import time
-
 import os
-import time
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 
 warnings.filterwarnings('ignore')
 
